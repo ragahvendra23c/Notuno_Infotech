cb707237d83dc9a14a3a6e04fd6f9a65ce68b0a2
Raghavendra98c <88829115+Raghavendra98c@users.noreply.github.com>
2023-09-26 21:39:19 +0530

    project
    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ProductItem from "./ProductItem";
+import withContext from "../withContext";
+
+const ProductList = props => {
+  const { products } = props.context;
+
+  return (
+    <>
+      
+      <br />
+      <div className="container">
+        <div className="column columns is-multiline">
+          {products && products.length ? (
+            products.map((product, index) => (
+              <ProductItem
+                product={product}
+                key={index}
+                addToCart={props.context.addToCart}
+              />
+            ))
+          ) : (
+            <div className="column">
+              <span className="title has-text-grey-light">
+                No products found!
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default withContext(ProductList);
